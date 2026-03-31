b298ead99d41ff161d321c6c28ec5e4d03b4c874
khansa-qonita <khansaqonita420@gmail.com>
2026-03-31 20:17:13 +0700

    menambah struktur perulangan FOR untuk cetak teks

--- looping.cpp
+++ looping.cpp
@@ -2,3 +2,12 @@
 #include <ctime>
 using namespace std;
 
+
+int main( ){
+    int x;
+//Perulangan dengan FOR
+//mencetak tulisan seabnyak 5 kali cout<<"PERULANGAN FOR"<<endl
+    for(int i=0;i<=4;1++){
+        cout<<"Teknologi Informasi UMY" <<endl;
+}
+cout<<endl;
